94ababbfacb235206c00d7216dd29d5fb76e7faf
SegunEmmanuel1 <segunemmanuel67@yahoo.com>
2024-06-06 23:47:20 +0000

    my new normal

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+e Puppet to run SED to change ".phpp" to ".php"
+exec { 'type fixer ftw':
+    command  => 'sed -i "s|class-wp-locale.phpp|class-wp-locale.php|g" /var/www/html/wp-settings.php',
+    provider => shell,
+}
